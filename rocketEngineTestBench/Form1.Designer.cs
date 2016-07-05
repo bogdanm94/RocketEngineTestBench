3c5375579a158bad18616bdf520e610f2bc05815
bogdanm94 <nmarbm@gmail.com>
2016-07-05 21:51:33 +0200

    Add files via upload

---
+++ rocketEngineTestBench/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace rocketEngineTestBench
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.handshakeLabel = new System.Windows.Forms.Label();
+            this.bitrateComboBox = new System.Windows.Forms.ComboBox();
+            this.portComboBox = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.BackColor = System.Drawing.Color.Red;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.startButton.ForeColor = System.Drawing.Color.Black;
+            this.startButton.Location = new System.Drawing.Point(733, 13);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(306, 162);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START ENGINE TEST";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.handshakeLabel);
+            this.groupBox1.Controls.Add(this.bitrateComboBox);
+            this.groupBox1.Controls.Add(this.portComboBox);
+            this.groupBox1.Controls.Add(this.connectButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(156, 162);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // handshakeLabel
+            // 
+            this.handshakeLabel.AutoSize = true;
+            this.handshakeLabel.Location = new System.Drawing.Point(7, 105);
+            this.handshakeLabel.Name = "handshakeLabel";
+            this.handshakeLabel.Size = new System.Drawing.Size(86, 13);
+            this.handshakeLabel.TabIndex = 3;
+            this.handshakeLabel.Text = "handshakeLabel";
+            // 
+            // bitrateComboBox
+            // 
+            this.bitrateComboBox.FormattingEnabled = true;
+            this.bitrateComboBox.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200"});
+            this.bitrateComboBox.Location = new System.Drawing.Point(7, 48);
+            this.bitrateComboBox.Name = "bitrateComboBox";
+            this.bitrateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.bitrateComboBox.TabIndex = 2;
+            // 
+            // portComboBox
+            // 
+            this.portComboBox.FormattingEnabled = true;
+            this.portComboBox.Location = new System.Drawing.Point(7, 20);
+            this.portComboBox.Name = "portComboBox";
+            this.portComboBox.Size = new System.Drawing.Size(121, 21);
+            this.portComboBox.TabIndex = 1;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(6, 75);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(122, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineColor = System.Drawing.Color.Gray;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.chart1.BorderlineWidth = 3;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 181);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "thrust";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1026, 554);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 36);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(540, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 50;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Location = new System.Drawing.Point(175, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(552, 162);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chart management";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "X axis increment";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(6, 98);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(540, 45);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 50;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ploting start sensitivity (grams)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(1046, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(146, 722);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "bmp";
+            this.saveFileDialog1.Filter = "Bitmap image (.bmp)|*.bmp";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1204, 747);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ComboBox bitrateComboBox;
+        private System.Windows.Forms.ComboBox portComboBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label handshakeLabel;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ rocketEngineTestBench/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace rocketEngineTestBench
+{
+    public partial class Form1 : Form
+    {
+
+        bool handshake = false;
+        string readLine;
+        int thrust;
+        bool testActive = false;
+        double time;
+        double xAxisIncrement;
+        int startingSensitivity;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+            bitrateComboBox.Text = "9600";
+            this.Select();
+            
+        }
+        
+        public void getAvailablePorts() //Get available ports
+        {
+            try
+            {
+               
+                    String[] availablePorts = SerialPort.GetPortNames();
+                    portComboBox.Items.Clear();
+                    portComboBox.Items.AddRange(availablePorts);
+            }
+            catch
+            {
+                MessageBox.Show("Can`t find available ports");
+            }
+
+        }
+
+        public void connectToArduino() //Connect to arduino
+        {
+
+            if (serialPort1.IsOpen)
+            {
+
+
+                //backgroundWorker2.CancelAsync();
+                serialPort1.Close();
+
+                if (!serialPort1.IsOpen)
+                {
+                    connectButton.BackColor = Color.OrangeRed;
+                    connectButton.Text = "Connect";
+                    handshake = false;
+                    handshakeLabel.Visible = false;
+                }
+            }
+            else
+            {
+                //if com port not selected display error
+                if (portComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Port not selected");
+                }
+                //if data rate not selected display error
+                else if (bitrateComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Data rate not selected");
+                }
+                //if port selected try to connect to it
+                else
+                {
+
+                    if (!serialPort1.IsOpen && portComboBox.SelectedText != null && bitrateComboBox.SelectedText != null)
+                    {
+                        //Set comport and bitrate
+                        serialPort1.PortName = portComboBox.Text;
+                        serialPort1.BaudRate = Convert.ToInt16(bitrateComboBox.Text);
+
+                        serialPort1.Open();
+
+                        if (serialPort1.IsOpen)
+                        {
+                            handshakeLabel.Visible = true;
+                            handshakeLabel.ForeColor = Color.Black;
+                            handshakeLabel.Text = "Waiting for handshake...";
+                            serialPort1.Write("?");
+                            serialPort1.DiscardOutBuffer();
+
+                            Thread.Sleep(1000);
+                            if (handshake == true)
+                            {
+                                handshakeLabel.ForeColor = Color.LimeGreen;
+                                handshakeLabel.Text = "Handshake OK";
+                                connectButton.BackColor = Color.LimeGreen;
+                                connectButton.Text = "Connected";
+                            }
+                            else
+                            {
+                                handshakeLabel.ForeColor = Color.Red;
+                                handshakeLabel.Text = "Handshake failed";
+
+                            }
+
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.ReadByte() == 'Y')
+            {
+                handshake = true;
+                serialPort1.DiscardInBuffer();
+            }
+            readLine = serialPort1.ReadLine();
+            serialPort1.DiscardInBuffer();
+            if(serialPort1.ReadByte() == 'S')
+            {
+                startButton.BackColor = Color.LimeGreen;
+                startButton.Text = "ENGINE IGNITED";
+            }
+            if (Int32.TryParse(readLine, out thrust))
+            {
+                if(thrust > startingSensitivity)
+                {
+                    testActive = true;
+                    startButton.Text = "ENGINE STARTED";
+                    time += xAxisIncrement;
+                    chart1.Series["thrust"].Points.AddXY(time, thrust);
+                    richTextBox1.AppendText(thrust.ToString() + "\n");
+
+                }
+                else if(testActive ==  true)
+                {
+                    testOver();
+                }
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            startingSensitivity = trackBar1.Value;
+            label3.Text = startingSensitivity.ToString();
+
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            xAxisIncrement = Convert.ToDouble(trackBar2.Value) / 100;
+            label4.Text = xAxisIncrement.ToString();
+            
+        }
+
+        
+        public void testOver()
+        {
+            testActive = false;
+            time = 0;
+            startButton.Text = "START ENGINE TEST";
+            startButton.BackColor = Color.Red;
+            saveFileDialog1.ShowDialog();
+
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen && handshake == true)
+            {
+                testActive = true;
+                serialPort1.Write("Start");
+            }
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (saveFileDialog1.CheckPathExists == true && saveFileDialog1.ValidateNames == true)
+            {
+                chart1.SaveImage(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(ModifierKeys == Keys.Shift && ModifierKeys == Keys.Enter)
+            {
+
+                startButton.PerformClick();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                getAvailablePorts();
+            }
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            chart1.Width = this.Width - 200;
+            chart1.Height = this.Height - 230;
+        }
+    }
+}
